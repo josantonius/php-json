c1360f8bb736978ddde029cd376f2053edabb8bc
Josantonius <info@josantonius.com>
2017-03-18 05:13:40 +0100

    The repository was updated to version 1.1.1

--- src/Json.php
+++ src/Json.php
@@ -2,7 +2,7 @@
 /**
  * PHP simple library for managing Json files.
  * 
- * @author     Josantonius - hola@josantonius.com
+ * @author     Josantonius - hello@josantonius.com
  * @copyright  Copyright (c) 2016 - 2017
  * @license    https://opensource.org/licenses/MIT - The MIT License (MIT)
  * @link       https://github.com/Josantonius/PHP-Json
