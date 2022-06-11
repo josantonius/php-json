442beea6028eb52b4bd7e115debe5ec38656b59c
Josantonius <hello@josantonius.dev>
2022-06-11 13:02:18 +0200

    refactor: update comments
    Update comments; website, urls...

--- src/Exception/JsonException.php
+++ src/Exception/JsonException.php
@@ -1,21 +1,22 @@
 <?php
+
 /**
- * PHP simple library for managing Json files.
+ * PHP simple library for managing JSON files.
  *
- * @author    Josantonius <hello@josantonius.com>
- * @copyright 2016 - 2018 (c) Josantonius - PHP-Json
+ * @author    Josantonius <hello@josantonius.dev>
+ * @copyright 2016 (c) Josantonius
  * @license   https://opensource.org/licenses/MIT - The MIT License (MIT)
- * @link      https://github.com/Josantonius/PHP-Json
+ * @link      https://github.com/josantonius/php-json
  * @since     1.0.0
  */
 namespace Josantonius\Json\Exception;
 
 /**
- * Exception class for Json library.
+ * Exception class for JSON library.
  *
  * You can use an exception and error handler with this library.
  *
- * @link https://github.com/Josantonius/PHP-ErrorHandler
+ * @link https://github.com/josantonius/php-errorhandler
  */
 class JsonException extends \Exception
 {
@@ -27,7 +28,7 @@ class JsonException extends \Exception
      */
     public function __construct($msg = '', $status = 0)
     {
-        $this->message = $msg;
+        $this->message    = $msg;
         $this->statusCode = $status;
     }
 }
--- src/JsonLastError.php
+++ src/JsonLastError.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
- * PHP simple library for managing Json files.
+ * PHP simple library for managing JSON files.
  *
- * @author    Josantonius <hello@josantonius.com>
- * @copyright 2016 - 2018 (c) Josantonius - PHP-Json
+ * @author    Josantonius <hello@josantonius.dev>
+ * @copyright 2016 (c) Josantonius
  * @license   https://opensource.org/licenses/MIT - The MIT License (MIT)
- * @link      https://github.com/Josantonius/PHP-Json
+ * @link      https://github.com/josantonius/php-json
  * @since     1.1.7
  */
 namespace Josantonius\Json;
@@ -37,52 +38,53 @@ class JsonLastError
     public static function getCollection()
     {
         $collections = [
-            JSON_ERROR_NONE => null,
-            JSON_ERROR_DEPTH => [
-                'message' => 'Maximum stack depth exceeded',
+            JSON_ERROR_NONE             => null,
+            JSON_ERROR_DEPTH            => [
+                'message'    => 'Maximum stack depth exceeded',
                 'error-code' => 1,
             ],
-            JSON_ERROR_STATE_MISMATCH => [
-                'message' => 'Underflow or the modes mismatch',
+            JSON_ERROR_STATE_MISMATCH   => [
+                'message'    => 'Underflow or the modes mismatch',
                 'error-code' => 2,
             ],
-            JSON_ERROR_CTRL_CHAR => [
-                'message' => 'Unexpected control char found',
+            JSON_ERROR_CTRL_CHAR        => [
+                'message'    => 'Unexpected control char found',
                 'error-code' => 3,
             ],
-            JSON_ERROR_SYNTAX => [
-                'message' => 'Syntax error, malformed JSON',
+            JSON_ERROR_SYNTAX           => [
+                'message'    => 'Syntax error, malformed JSON',
                 'error-code' => 4,
             ],
-            JSON_ERROR_UTF8 => [
-                'message' => 'Malformed UTF-8 characters',
+            JSON_ERROR_UTF8             => [
+                'message'    => 'Malformed UTF-8 characters',
                 'error-code' => 5,
             ],
-            JSON_ERROR_RECURSION => [
-                'message' => 'Recursion error in value to be encoded',
+            JSON_ERROR_RECURSION        => [
+                'message'    => 'Recursion error in value to be encoded',
                 'error-code' => 6,
             ],
-            JSON_ERROR_INF_OR_NAN => [
-                'message' => 'Error NAN/INF in value to be encoded',
+            JSON_ERROR_INF_OR_NAN       => [
+                'message'    => 'Error NAN/INF in value to be encoded',
                 'error-code' => 7,
             ],
             JSON_ERROR_UNSUPPORTED_TYPE => [
-                'message' => 'Type value given cannot be encoded',
+                'message'    => 'Type value given cannot be encoded',
                 'error-code' => 8,
             ],
-            'default' => [
-                'message' => 'Unknown error',
+            'default'                   => [
+                'message'    => 'Unknown error',
                 'error-code' => 999,
             ],
         ];
 
         if (version_compare(PHP_VERSION, '7.0.0', '>=')) {
             $collections[JSON_ERROR_INVALID_PROPERTY_NAME] = [
-                'message' => 'Name value given cannot be encoded',
+                'message'    => 'Name value given cannot be encoded',
                 'error-code' => 9,
             ];
+
             $collections[JSON_ERROR_UTF16] = [
-                'message' => 'Malformed UTF-16 characters',
+                'message'    => 'Malformed UTF-16 characters',
                 'error-code' => 10,
             ];
         }
