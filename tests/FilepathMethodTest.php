858f59e55f1374b4c6d4c69b59d35c0c1c47a451
Josantonius <hello@josantonius.dev>
2022-09-05 20:01:54 +0200

    test: upgrade to the new version
    Closes #22

---
+++ tests/FilepathMethodTest.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of https://github.com/josantonius/php-json repository.
+ *
+ * (c) Josantonius <hello@josantonius.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @phpcs:disable PSR1.Methods.CamelCapsMethodName.NotCamelCaps
+ */
+
+namespace Josantonius\Json\Tests;
+
+use Josantonius\Json\Json;
+use PHPUnit\Framework\TestCase;
+
+class FilepathMethodTest extends TestCase
+{
+    private string $filepath =  __DIR__ . '/filename.json';
+
+    private string $url = 'https://raw.githubusercontent.com/josantonius/php-json/main/composer.json';
+
+    public function setUp(): void
+    {
+        parent::setup();
+    }
+
+    public function tearDown(): void
+    {
+        if (file_exists($this->filepath)) {
+            unlink($this->filepath);
+        }
+    }
+
+    public function test_should_get_the_path_to_a_local_file(): void
+    {
+        $jsonFile = new Json($this->filepath);
+
+        $this->assertEquals($this->filepath, $jsonFile->filepath());
+    }
+
+    public function test_should_get_the_path_to_a_remote_file(): void
+    {
+        $jsonFile = new Json($this->url);
+
+        $this->assertEquals($this->url, $jsonFile->filepath());
+    }
+}
